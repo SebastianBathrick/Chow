d98f0fd9b0366b7c7b03f9b93ce779410144eab4
Sebastian Bathrick <72719337+SebastianBathrick@users.noreply.github.com>
2026-04-30 23:34:40 -0400

    Add project files.

---
+++ src/Chow/ChowRuntime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chow.V2
+{
+    public static class ChowRuntime
+    {
+        public static ChowValue ExecuteCode(string sourceCode)
+        {
+            return null; // Placeholder
+        }
+    }`
+}
