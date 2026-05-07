5d17dd636947d4d4ec97a8decdf1868efa0f59e1
Sebastian Bathrick <72719337+SebastianBathrick@users.noreply.github.com>
2026-05-07 18:32:13 -0400

    Add REPL line indicators

--- src/Chow.Cli/Program.cs
+++ src/Chow.Cli/Program.cs
@@ -1,6 +1,8 @@
 using Chow.Interpreter;
 
 const char NEWLINE_CHAR = '\n';
+const string START_INDICATOR = ">>> ";
+const string TRAILING_INDICATOR = "... ";
 
 ChowModule module = new ChowModule();
 
@@ -8,7 +10,7 @@ while (true)
 {
     try
     {
-        var srcCode = string.Join(NEWLINE_CHAR, GetLineList());
+        var srcCode = string.Join(NEWLINE_CHAR, GetLineList(START_INDICATOR, TRAILING_INDICATOR));
         module.Run(srcCode);
     }
     catch (Exception ex)
@@ -17,13 +19,23 @@ while (true)
     }
 }
 
-static List<string> GetLineList()
+static List<string> GetLineList(string startIndicator, string trailingIndicator)
 {
+    if (startIndicator.Length != trailingIndicator.Length)
+    {
+        throw new ArgumentException(nameof(trailingIndicator));
+    }
+
+    var prefixLength = startIndicator.Length;
+
     // Starts at the beginning of line 1
     var lines = new List<string>() { string.Empty };
     var currLine = 0;
     var cursorY = Console.CursorTop;
     var cursorX = 0;
+
+    Console.SetCursorPosition(0, cursorY);
+    Console.Write(startIndicator);
 
     ConsoleKeyInfo keyInfo;
     bool isSubmitting = false;
@@ -44,23 +56,26 @@ static List<string> GetLineList()
                     // ENTER + SHIFT = move cursor to newline and add a new line to the list
                     lines.Add(string.Empty);
                     currLine++;
+                    cursorY++;
+                    Console.SetCursorPosition(0, cursorY);
+                    Console.Write(trailingIndicator);
                 }
                 else
                 {
                     // ENTER = move cursor to new line
                     isSubmitting = true;
+                    cursorY++;
                 }
-
-                cursorY++;
                 break;
 
-            case ConsoleKey.L:
+            case ConsoleKey.L when keyInfo.Modifiers == ConsoleModifiers.Control:
                 Console.Clear();
-                cursorY = cursorY - Console.CursorTop;
-                
+                cursorY = currLine;
+
                 for(int i = 0; i < lines.Count; i++)
                 {
-                    RedrawLine(lines[i], cursorY);
+                    var redrawPrefix = i == 0 ? startIndicator : trailingIndicator;
+                    RedrawLine(lines[i], i, redrawPrefix);
                 }
                 break;
 
@@ -74,11 +89,11 @@ static List<string> GetLineList()
                 cursorX--;
                 var backspacedLine = lines[currLine].Remove(cursorX , 1);
 
-                RedrawLine(backspacedLine, cursorY);
+                RedrawLine(backspacedLine, cursorY, currLine == 0 ? startIndicator : trailingIndicator);
 
                 lines[currLine] = backspacedLine;
                 break;
-            
+
             case ConsoleKey.LeftArrow:
                 if (cursorX > 0)
                 {
@@ -103,7 +118,7 @@ static List<string> GetLineList()
                 else
                 {
                     var insertedLine = lines[currLine].Insert(cursorX, keyInfo.KeyChar.ToString());
-                    RedrawLine(insertedLine, cursorY);
+                    RedrawLine(insertedLine, cursorY, currLine == 0 ? startIndicator : trailingIndicator);
                     lines[currLine] = insertedLine;
                     cursorX++;
                 }
@@ -123,7 +138,7 @@ static List<string> GetLineList()
                 else
                 {
                     var insertedLine = lines[currLine].Insert(cursorX, keyInfo.KeyChar.ToString());
-                    RedrawLine(insertedLine, cursorY);
+                    RedrawLine(insertedLine, cursorY, currLine == 0 ? startIndicator : trailingIndicator);
                     lines[currLine] = insertedLine;
                 }
 
@@ -131,22 +146,23 @@ static List<string> GetLineList()
                 break;
         }
 
-        Console.SetCursorPosition(cursorX, cursorY);
+        Console.SetCursorPosition(cursorX + prefixLength, cursorY);
     }
     while (!isSubmitting);
 
     return lines;
 }
 
-static void RedrawLine(string line, int cursorY)
+static void RedrawLine(string line, int cursorY, string prefix)
 {
-    // Set cursor before loop to account line.Length = 1
     Console.SetCursorPosition(0, cursorY);
-    var redrawCursorX = 1;
+    Console.Write(prefix);
+
+    var redrawCursorX = 0;
 
     while (redrawCursorX < line.Length)
     {
-        Console.SetCursorPosition(redrawCursorX, cursorY);
+        Console.SetCursorPosition(redrawCursorX + prefix.Length, cursorY);
 
         var posDrawChar = line[redrawCursorX++];
         Console.Write(posDrawChar);
