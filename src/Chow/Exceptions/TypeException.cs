8ffa1dd82880503aa087125b7ae77bf02853e1d2
Sebastian Bathrick <72719337+SebastianBathrick@users.noreply.github.com>
2026-06-04 11:08:53 -0400

    Update TypeException

--- src/Chow/Exceptions/TypeException.cs
+++ src/Chow/Exceptions/TypeException.cs
@@ -1,14 +1,25 @@
 using System;
+using Chow.DataTypes;
 namespace Chow.Exceptions
 {
     class TypeException : Exception
-    {
+    {      
         public TypeException()
         {
         }
 
         public TypeException(string message) : base(message)
         {
+        }
+        
+        
+        public TypeException(Tag lTag, Tag rTag, ExpressionOperator @operator) : base(CreateOperandTypesMessage(lTag, rTag, @operator))
+        {
+        }
+
+        static string CreateOperandTypesMessage(Tag lTag, Tag rTag)
+        {
+            return $"TypeError: unsupported operand type(s) for {op}: '{lTag}' and '{rTag}'";
         }
     }
 }
