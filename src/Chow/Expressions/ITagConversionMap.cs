bac986e80ae659b4e5f647850cbbb712bcfb4ce2
Sebastian Bathrick <72719337+SebastianBathrick@users.noreply.github.com>
2026-06-05 12:34:12 -0400

    Added ITagConversionMap

---
+++ src/Chow/Expressions/ITagConversionMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Chow.Expressions
+{
+    interface ITagConversionMap
+    {
+        IReadOnlyDictionary<(Tag, Tag), Tag> ConversionRules { get; }
+    }
+}
