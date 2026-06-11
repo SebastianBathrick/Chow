26b405df41cae8dd6297fa8241fee3a62a640409
Sebastian Bathrick <72719337+SebastianBathrick@users.noreply.github.com>
2026-06-11 03:09:51 -0400

    Add TokenStream and Tests

---
+++ src/Chow/Tokens/TokenStream.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+
+namespace Chow.Tokens
+{
+    /// <summary>
+    /// A forward-only stream of scanner tokens that are selected and consumed one at a time.
+    /// </summary>
+    class TokenStream : ITokenStream
+    {
+        const int InitialTokenIndex = 0;
+        
+        List<Token> _tokensList;
+        int _tokenIdx = InitialTokenIndex;
+        
+        Token SelectedToken => _tokensList[_tokenIdx];
+
+        /// <summary>The line number of the selected token.</summary>
+        public int LineNumber => SelectedToken.LineNumber;
+        
+        /// <summary>Whether there are any more tokens to consume.</summary>
+        public bool IsEndOfStream => _tokenIdx == _tokensList.Count;
+
+        /// <summary>Creates a token stream over the given list of tokens.</summary>
+        /// <param name="tokensList">The tokens this stream will select from, in order.</param>
+        /// <remarks>This constructor is temporary and will be removed after the scanner
+        /// refactor.</remarks>
+        public TokenStream(List<Token> tokensList)
+        {
+            _tokensList = tokensList;
+        }
+        
+        /// <summary>Selects the next token or reaches the end of this stream.</summary>
+        public Token Consume()
+        {
+            return _tokensList[_tokenIdx++];
+        }
+
+        /// <summary>
+        /// Selects the next token in the stream if the currently selected token is
+        /// <paramref name="expectedType"/>; otherwise, an exception will be thrown.
+        /// </summary>
+        /// <param name="expectedType">The token type the selected token's type will be compared
+        /// to.</param>
+        /// <returns>The token selected before this method is called.</returns>
+        /// <exception cref="SyntaxException">An exception containing the expected token type and
+        /// the line of the token selected before this method is called.</exception>
+        public Token ConsumeMatch(TokenType expectedType)
+        {
+            if (IsMatch(expectedType))
+            {
+                return Consume();
+            }
+            
+            var exLineNum = IsEndOfStream ? _tokensList[_tokenIdx - 1].LineNumber : LineNumber;
+            throw new SyntaxException(expectedType.ToString(), exLineNum);
+        }
+
+        /// <summary>
+        /// Determines whether the selected token is <paramref name="targetType"/>.
+        /// </summary>
+        /// <param name="targetType">The token type the selected token's type will be compared
+        /// to.</param>
+        /// <returns><c>true</c> if the selected token is <paramref name="targetType"/>;
+        /// <c>false</c> if it is not or the end of this stream has been reached.</returns>
+        public bool IsMatch(TokenType targetType)
+        {
+            return !IsEndOfStream && SelectedToken.Type == targetType;
+        }
+
+        /// <summary>
+        /// Determines whether the selected token is any of <paramref name="targetTypes"/>.
+        /// </summary>
+        /// <param name="targetTypes">The token types the selected token's type will be compared
+        /// to.</param>
+        /// <returns><c>true</c> if the selected token is any of <paramref name="targetTypes"/>;
+        /// <c>false</c> if it is none of them or the end of this stream has been reached.</returns>
+        public bool IsMatch(params TokenType[] targetTypes)
+        {
+            foreach (var type in targetTypes)
+            {
+                if (IsMatch(type))
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the token after the selected token is <paramref name="targetType"/>,
+        /// without selecting it.
+        /// </summary>
+        /// <param name="targetType">The token type the next token's type will be compared
+        /// to.</param>
+        /// <returns><c>true</c> if the token after the selected token is
+        /// <paramref name="targetType"/>; <c>false</c> if it is not or no token follows the selected
+        /// token.</returns>
+        public bool IsNextMatch(TokenType targetType)
+        {
+            return _tokenIdx + 1 < _tokensList.Count 
+                && _tokensList[_tokenIdx + 1].Type == targetType;
+        }
+
+        /// <summary>
+        /// Selects the next token in the stream if the currently selected token is
+        /// <paramref name="targetType"/>; otherwise, this stream is left unchanged.
+        /// </summary>
+        /// <param name="targetType">The token type the selected token's type will be compared
+        /// to.</param>
+        /// <returns><c>true</c> if a token was consumed; otherwise, <c>false</c>.</returns>
+        public bool TryConsumeMatch(TokenType targetType)
+        {
+            return TryConsumeMatch(targetType, out _);
+        }
+
+        /// <summary>
+        /// Selects the next token in the stream if the currently selected token is
+        /// <paramref name="targetType"/>; otherwise, this stream is left unchanged.
+        /// </summary>
+        /// <param name="targetType">The token type the selected token's type will be compared
+        /// to.</param>
+        /// <param name="token">The consumed token, or <c>default</c> if no token was
+        /// consumed.</param>
+        /// <returns><c>true</c> if a token was consumed; otherwise, <c>false</c>.</returns>
+        public bool TryConsumeMatch(TokenType targetType, out Token token)
+        {
+            if (IsEndOfStream || !IsMatch(targetType))
+            {
+                token = default;
+                return false;
+            }
+
+            token = Consume();
+            return true;
+        }
+
+        /// <summary>
+        /// Selects the next token in the stream if the currently selected token is any of
+        /// <paramref name="targetTypes"/>; otherwise, this stream is left unchanged.
+        /// </summary>
+        /// <param name="targetTypes">The token types the selected token's type will be compared
+        /// to.</param>
+        /// <returns><c>true</c> if a token was consumed; otherwise, <c>false</c>.</returns>
+        public bool TryConsumeMatch(params TokenType[] targetTypes)
+        {
+            if (IsEndOfStream)
+            {
+                return false;
+            }
+            
+            foreach (var type in targetTypes)
+            {
+                if (IsMatch(type))
+                {
+                    Consume();
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ tests/Chow.Tests.SourceCode/UnitTests/TokenStreamTests.cs
@@ -0,0 +1,269 @@
+using Chow;
+using Chow.Tokens;
+
+namespace Chow.Tests.SourceCode.UnitTests;
+
+public class TokenStreamTests
+{
+    [Test]
+    public void LineNumber_NewStream_ReturnsSelectedTokenLineNumber()
+    {
+        var stream = CreateStream(
+            Token(TokenType.Identifier, lineNumber: 12),
+            Token(TokenType.EndOfCode, lineNumber: 13));
+
+        Assert.That(stream.LineNumber, Is.EqualTo(12));
+    }
+
+    [Test]
+    public void Consume_SelectedToken_ReturnsTokenAndAdvancesStream()
+    {
+        var firstToken = Token(TokenType.Identifier, "value", 4);
+        var secondToken = Token(TokenType.EndOfCode, lineNumber: 5);
+        var stream = CreateStream(firstToken, secondToken);
+
+        var consumedToken = stream.Consume();
+
+        Assert.That(consumedToken, Is.EqualTo(firstToken));
+        Assert.That(stream.LineNumber, Is.EqualTo(5));
+        Assert.That(stream.IsMatch(TokenType.EndOfCode), Is.True);
+    }
+
+    [Test]
+    public void Consume_LastToken_ReachesEndOfStream()
+    {
+        var stream = CreateStream(Token(TokenType.EndOfCode, lineNumber: 1));
+
+        stream.Consume();
+
+        Assert.That(stream.IsEndOfStream, Is.True);
+        Assert.That(stream.IsMatch(TokenType.EndOfCode), Is.False);
+        Assert.That(stream.IsNextMatch(TokenType.EndOfCode), Is.False);
+    }
+
+    [Test]
+    public void ConsumeMatch_SelectedTokenMatches_ReturnsTokenAndAdvancesStream()
+    {
+        var firstToken = Token(TokenType.SymbolLeftParen, "(", 2);
+        var stream = CreateStream(
+            firstToken,
+            Token(TokenType.SymbolRightParen, ")", 2));
+
+        var consumedToken = stream.ConsumeMatch(TokenType.SymbolLeftParen);
+
+        Assert.That(consumedToken, Is.EqualTo(firstToken));
+        Assert.That(stream.IsMatch(TokenType.SymbolRightParen), Is.True);
+    }
+
+    [Test]
+    public void ConsumeMatch_SelectedTokenDoesNotMatch_ThrowsSyntaxExceptionWithSelectedLineNumber()
+    {
+        var stream = CreateStream(Token(TokenType.Identifier, "name", 7));
+
+        var exception = Assert.Throws<SyntaxException>(
+            () => stream.ConsumeMatch(TokenType.SymbolLeftParen));
+
+        Assert.That(exception, Is.Not.Null);
+        Assert.That(exception!.LineNumber, Is.EqualTo(7));
+        Assert.That(exception.Message, Is.EqualTo("SyntaxError: expected 'SymbolLeftParen'"));
+        Assert.That(stream.IsMatch(TokenType.Identifier), Is.True);
+    }
+
+    [Test]
+    public void ConsumeMatch_EndOfStream_ThrowsSyntaxExceptionWithPreviousTokenLineNumber()
+    {
+        var stream = CreateStream(Token(TokenType.EndOfCode, lineNumber: 9));
+        stream.Consume();
+
+        var exception = Assert.Throws<SyntaxException>(
+            () => stream.ConsumeMatch(TokenType.SymbolLeftParen));
+
+        Assert.That(exception, Is.Not.Null);
+        Assert.That(exception!.LineNumber, Is.EqualTo(9));
+        Assert.That(exception.Message, Is.EqualTo("SyntaxError: expected 'SymbolLeftParen'"));
+    }
+
+    [Test]
+    public void IsMatch_SelectedTokenMatchesTarget_ReturnsTrue()
+    {
+        var stream = CreateStream(Token(TokenType.SymbolPlus));
+
+        Assert.That(stream.IsMatch(TokenType.SymbolPlus), Is.True);
+    }
+
+    [Test]
+    public void IsMatch_SelectedTokenDoesNotMatchTarget_ReturnsFalse()
+    {
+        var stream = CreateStream(Token(TokenType.SymbolPlus));
+
+        Assert.That(stream.IsMatch(TokenType.SymbolMinus), Is.False);
+    }
+
+    [Test]
+    public void IsMatch_SelectedTokenMatchesAnyTarget_ReturnsTrue()
+    {
+        var stream = CreateStream(Token(TokenType.SymbolPlus));
+
+        var isMatch = stream.IsMatch(
+            TokenType.SymbolMinus,
+            TokenType.SymbolPlus,
+            TokenType.SymbolMultiply);
+
+        Assert.That(isMatch, Is.True);
+    }
+
+    [Test]
+    public void IsMatch_SelectedTokenMatchesNoTarget_ReturnsFalse()
+    {
+        var stream = CreateStream(Token(TokenType.SymbolPlus));
+
+        var isMatch = stream.IsMatch(TokenType.SymbolMinus, TokenType.SymbolMultiply);
+
+        Assert.That(isMatch, Is.False);
+    }
+
+    [Test]
+    public void IsNextMatch_NextTokenMatchesTarget_ReturnsTrueWithoutAdvancingStream()
+    {
+        var stream = CreateStream(
+            Token(TokenType.Identifier, "name", 3),
+            Token(TokenType.SymbolAssign, "=", 3));
+
+        var isNextMatch = stream.IsNextMatch(TokenType.SymbolAssign);
+
+        Assert.That(isNextMatch, Is.True);
+        Assert.That(stream.IsMatch(TokenType.Identifier), Is.True);
+    }
+
+    [Test]
+    public void IsNextMatch_NoTokenFollowsSelectedToken_ReturnsFalse()
+    {
+        var stream = CreateStream(Token(TokenType.Identifier));
+
+        Assert.That(stream.IsNextMatch(TokenType.SymbolAssign), Is.False);
+    }
+
+    [Test]
+    public void TryConsumeMatch_SelectedTokenMatchesTarget_ReturnsTrueAndOutputsToken()
+    {
+        var firstToken = Token(TokenType.KeywordIf, "if", 6);
+        var stream = CreateStream(firstToken, Token(TokenType.EndOfCode, lineNumber: 6));
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.KeywordIf, out var consumedToken);
+
+        Assert.That(wasConsumed, Is.True);
+        Assert.That(consumedToken, Is.EqualTo(firstToken));
+        Assert.That(stream.IsMatch(TokenType.EndOfCode), Is.True);
+    }
+
+    [Test]
+    public void TryConsumeMatchWithoutOutParam_SelectedTokenMatchesTarget_ReturnsTrueAndAdvancesStream()
+    {
+        var stream = CreateStream(
+            Token(TokenType.KeywordIf, "if", 6),
+            Token(TokenType.EndOfCode, lineNumber: 6));
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.KeywordIf);
+
+        Assert.That(wasConsumed, Is.True);
+        Assert.That(stream.IsMatch(TokenType.EndOfCode), Is.True);
+    }
+
+    [Test]
+    public void TryConsumeMatchWithoutOutParam_SelectedTokenDoesNotMatchTarget_ReturnsFalseAndLeavesStreamUnchanged()
+    {
+        var stream = CreateStream(Token(TokenType.KeywordIf, "if", 6));
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.KeywordElse);
+
+        Assert.That(wasConsumed, Is.False);
+        Assert.That(stream.IsMatch(TokenType.KeywordIf), Is.True);
+    }
+
+    [Test]
+    public void TryConsumeMatchWithoutOutParam_EndOfStream_ReturnsFalse()
+    {
+        var stream = CreateStream(Token(TokenType.EndOfCode, lineNumber: 1));
+        stream.Consume();
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.EndOfCode);
+
+        Assert.That(wasConsumed, Is.False);
+    }
+
+    [Test]
+    public void TryConsumeMatch_SelectedTokenDoesNotMatchTarget_ReturnsFalseAndLeavesStreamUnchanged()
+    {
+        var stream = CreateStream(Token(TokenType.KeywordIf, "if", 6));
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.KeywordElse, out var consumedToken);
+
+        Assert.That(wasConsumed, Is.False);
+        Assert.That(consumedToken, Is.EqualTo(default(Token)));
+        Assert.That(stream.IsMatch(TokenType.KeywordIf), Is.True);
+    }
+
+    [Test]
+    public void TryConsumeMatch_EndOfStream_ReturnsFalseAndOutputsDefaultToken()
+    {
+        var stream = CreateStream(Token(TokenType.EndOfCode, lineNumber: 1));
+        stream.Consume();
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.EndOfCode, out var consumedToken);
+
+        Assert.That(wasConsumed, Is.False);
+        Assert.That(consumedToken, Is.EqualTo(default(Token)));
+    }
+
+    [Test]
+    public void TryConsumeMatch_SelectedTokenMatchesAnyTarget_ReturnsTrueAndAdvancesStream()
+    {
+        var stream = CreateStream(
+            Token(TokenType.SymbolMinus, "-", 8),
+            Token(TokenType.LiteralInt, "1", 8, 1L));
+
+        var wasConsumed = stream.TryConsumeMatch(
+            TokenType.SymbolPlus,
+            TokenType.SymbolMinus,
+            TokenType.SymbolMultiply);
+
+        Assert.That(wasConsumed, Is.True);
+        Assert.That(stream.IsMatch(TokenType.LiteralInt), Is.True);
+    }
+
+    [Test]
+    public void TryConsumeMatch_SelectedTokenMatchesNoTarget_ReturnsFalseAndLeavesStreamUnchanged()
+    {
+        var stream = CreateStream(Token(TokenType.SymbolMinus, "-", 8));
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.SymbolPlus, TokenType.SymbolMultiply);
+
+        Assert.That(wasConsumed, Is.False);
+        Assert.That(stream.IsMatch(TokenType.SymbolMinus), Is.True);
+    }
+
+    [Test]
+    public void TryConsumeMatch_AnyTargetAtEndOfStream_ReturnsFalse()
+    {
+        var stream = CreateStream(Token(TokenType.EndOfCode, lineNumber: 1));
+        stream.Consume();
+
+        var wasConsumed = stream.TryConsumeMatch(TokenType.EndOfCode, TokenType.EmptyToken);
+
+        Assert.That(wasConsumed, Is.False);
+    }
+
+    static TokenStream CreateStream(params Token[] tokens)
+    {
+        return new TokenStream(tokens.ToList());
+    }
+
+    static Token Token(
+        TokenType type,
+        string lexeme = "",
+        int lineNumber = 1,
+        object? literal = null)
+    {
+        return new Token(type, lexeme, lineNumber, literal);
+    }
+}
