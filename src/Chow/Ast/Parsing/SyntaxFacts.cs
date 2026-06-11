5c457e76dfe4263cf3609b0ef2888a372af0ca41
Sebastian Bathrick <72719337+SebastianBathrick@users.noreply.github.com>
2026-06-11 14:16:45 -0400

    Add SyntaxFacts & unit tests

---
+++ src/Chow/Ast/Parsing/SyntaxFacts.cs
@@ -0,0 +1,105 @@
+﻿using Chow.Tokens;
+using Chow.Utility;
+
+namespace Chow.Ast.Parsing
+{
+    static class SyntaxFacts
+    {
+        static readonly TokenType[] ComparisonOperatorTypes =
+        {
+            TokenType.SymbolEqualTo,
+            TokenType.SymbolNotEqual,
+            TokenType.SymbolLess,
+            TokenType.SymbolGreater,
+            TokenType.SymbolLessEqual,
+            TokenType.SymbolGreaterEqual,
+            TokenType.KeywordIn
+        };
+        
+        static readonly TokenType[] ExpressionStartTypes =
+        {
+            TokenType.Name,
+            TokenType.LiteralInt,
+            TokenType.LiteralFloat,
+            TokenType.LiteralStr,
+            TokenType.LiteralFString,
+            TokenType.KeywordNone,
+            TokenType.KeywordTrue,
+            TokenType.KeywordFalse,
+            TokenType.KeywordNot,
+            TokenType.SymbolLeftParen,
+            TokenType.SymbolLeftBracket,
+            TokenType.SymbolMinus,
+            TokenType.SymbolLeftCurly
+        };
+
+        public static bool IsExpressionStart(TokenType checkType)
+        {
+            foreach (var type in ExpressionStartTypes)
+            {
+                if (type == checkType)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+        public static bool IsComparisonOperator(TokenType checkType)
+        {
+            foreach (var type in ComparisonOperatorTypes)
+            {
+                if (type == checkType)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static Operator ToBinaryOperator(TokenType operatorType)
+        {
+            switch (operatorType)
+            {
+                case TokenType.SymbolPlus:
+                    return Operator.Add;
+                case TokenType.SymbolMinus:
+                    return Operator.Subtract;
+                case TokenType.SymbolMultiply:
+                    return Operator.Multiply;
+                case TokenType.SymbolDivide:
+                    return Operator.Divide;
+                case TokenType.SymbolPercent:
+                    return Operator.Modulus;
+                case TokenType.SymbolExponent:
+                    return Operator.Exponentiate;
+                case TokenType.SymbolFloorDivide:
+                    return Operator.FloorDivide;
+                case TokenType.SymbolEqualTo:
+                    return Operator.Equal;
+                case TokenType.SymbolNotEqual:
+                    return Operator.NotEqual;
+                case TokenType.SymbolLess:
+                    return Operator.Less;
+                case TokenType.SymbolGreater:
+                    return Operator.Greater;
+                case TokenType.SymbolLessEqual:
+                    return Operator.LessEqual;
+                case TokenType.SymbolGreaterEqual:
+                    return Operator.GreaterEqual;
+                case TokenType.KeywordAnd:
+                    return Operator.And;
+                case TokenType.KeywordOr:
+                    return Operator.Or;
+                case TokenType.SymbolPipe:
+                    return Operator.BinaryOr;
+                case TokenType.KeywordIn:
+                    return Operator.In;
+                default:
+                    throw new UnreachableException(nameof(ToBinaryOperator));
+            }
+        }
+    }
+}
---
+++ tests/Chow.Tests.SourceCode/UnitTests/SyntaxFactsTests.cs
@@ -0,0 +1,149 @@
+using Chow;
+using Chow.Ast.Parsing;
+using Chow.Tokens;
+using Chow.Utility;
+
+namespace Chow.Tests.SourceCode.UnitTests;
+
+public class SyntaxFactsTests
+{
+    [Test]
+    public void IsExpressionStart_TokenTypeCanBeginExpression_ReturnsTrue()
+    {
+        var startTypes = new[]
+        {
+            TokenType.Name,
+            TokenType.LiteralInt,
+            TokenType.LiteralFloat,
+            TokenType.LiteralStr,
+            TokenType.LiteralFString,
+            TokenType.KeywordNone,
+            TokenType.KeywordTrue,
+            TokenType.KeywordFalse,
+            TokenType.KeywordNot,
+            TokenType.SymbolLeftParen,
+            TokenType.SymbolLeftBracket,
+            TokenType.SymbolMinus,
+            TokenType.SymbolLeftCurly
+        };
+
+        Assert.Multiple(() =>
+        {
+            foreach (var type in startTypes)
+            {
+                Assert.That(SyntaxFacts.IsExpressionStart(type), Is.True, type.ToString());
+            }
+        });
+    }
+
+    [Test]
+    public void IsExpressionStart_TokenTypeCannotBeginExpression_ReturnsFalse()
+    {
+        var nonStartTypes = new[]
+        {
+            TokenType.SymbolAssign,
+            TokenType.SymbolPlus,
+            TokenType.SymbolRightParen,
+            TokenType.KeywordIf,
+            TokenType.KeywordIn,
+            TokenType.Newline,
+            TokenType.EndOfCode
+        };
+
+        Assert.Multiple(() =>
+        {
+            foreach (var type in nonStartTypes)
+            {
+                Assert.That(SyntaxFacts.IsExpressionStart(type), Is.False, type.ToString());
+            }
+        });
+    }
+
+    [Test]
+    public void IsComparisonOperator_ComparisonTokenType_ReturnsTrue()
+    {
+        var comparisonTypes = new[]
+        {
+            TokenType.SymbolEqualTo,
+            TokenType.SymbolNotEqual,
+            TokenType.SymbolLess,
+            TokenType.SymbolGreater,
+            TokenType.SymbolLessEqual,
+            TokenType.SymbolGreaterEqual,
+            TokenType.KeywordIn
+        };
+
+        Assert.Multiple(() =>
+        {
+            foreach (var type in comparisonTypes)
+            {
+                Assert.That(SyntaxFacts.IsComparisonOperator(type), Is.True, type.ToString());
+            }
+        });
+    }
+
+    [Test]
+    public void IsComparisonOperator_NonComparisonTokenType_ReturnsFalse()
+    {
+        var nonComparisonTypes = new[]
+        {
+            TokenType.SymbolAssign,
+            TokenType.SymbolPlus,
+            TokenType.KeywordNot,
+            TokenType.KeywordAnd,
+            TokenType.Name
+        };
+
+        Assert.Multiple(() =>
+        {
+            foreach (var type in nonComparisonTypes)
+            {
+                Assert.That(SyntaxFacts.IsComparisonOperator(type), Is.False, type.ToString());
+            }
+        });
+    }
+
+    [Test]
+    public void ToBinaryOperator_OperatorTokenType_ReturnsMappedOperator()
+    {
+        var expectedMappings = new Dictionary<TokenType, Operator>
+        {
+            [TokenType.SymbolPlus] = Operator.Add,
+            [TokenType.SymbolMinus] = Operator.Subtract,
+            [TokenType.SymbolMultiply] = Operator.Multiply,
+            [TokenType.SymbolDivide] = Operator.Divide,
+            [TokenType.SymbolPercent] = Operator.Modulus,
+            [TokenType.SymbolExponent] = Operator.Exponentiate,
+            [TokenType.SymbolFloorDivide] = Operator.FloorDivide,
+            [TokenType.SymbolEqualTo] = Operator.Equal,
+            [TokenType.SymbolNotEqual] = Operator.NotEqual,
+            [TokenType.SymbolLess] = Operator.Less,
+            [TokenType.SymbolGreater] = Operator.Greater,
+            [TokenType.SymbolLessEqual] = Operator.LessEqual,
+            [TokenType.SymbolGreaterEqual] = Operator.GreaterEqual,
+            [TokenType.KeywordAnd] = Operator.And,
+            [TokenType.KeywordOr] = Operator.Or,
+            [TokenType.SymbolPipe] = Operator.BinaryOr,
+            [TokenType.KeywordIn] = Operator.In
+        };
+
+        Assert.Multiple(() =>
+        {
+            foreach (var mapping in expectedMappings)
+            {
+                Assert.That(
+                    SyntaxFacts.ToBinaryOperator(mapping.Key),
+                    Is.EqualTo(mapping.Value),
+                    mapping.Key.ToString());
+            }
+        });
+    }
+
+    [Test]
+    public void ToBinaryOperator_NonOperatorTokenType_ThrowsUnreachableException()
+    {
+        Assert.That(
+            () => SyntaxFacts.ToBinaryOperator(TokenType.Name),
+            Throws.TypeOf<UnreachableException>());
+    }
+}
